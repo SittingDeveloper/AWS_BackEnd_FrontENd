bf00d81b04bf2309fa036818f5af5a32478a810a
SittingDeveloper <rlaaudtlr145@gmail.com>
2023-04-20 17:55:25 +0900

    AWS BackEnd 배포 완료

---
+++ BackEnd/src/main/java/com/example/demo/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    // "/" , "/auth/**" 등은 WebSecurityConfig에서 인증하지 않아도 되도록 설정해서 인증필요없음
+    @GetMapping("/")
+    public String healthCheck() {
+        return "The Service is up and running ... ";
+    }
+}
