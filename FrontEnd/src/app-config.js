d8bd40f95341efb42026a8565595fe83d542bb5c
SittingDeveloper <rlaaudtlr145@gmail.com>
2023-04-21 11:54:38 +0900

    commit for delpoyment

--- FrontEnd/src/app-config.js
+++ FrontEnd/src/app-config.js
@@ -1,9 +1,11 @@
-/*let backendHost;
+let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
     backendHost = "http://localhost:8080";
-}*/
+} else {
+    backendHost = "http://kms-aws-practice.ap-northeast-2.elasticbeanstalk.com/";
+}
 
-export const API_BASE_URL = "http://localhost:8080/";
+export const API_BASE_URL = `${backendHost}`;
