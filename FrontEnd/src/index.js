8045a2611de534458c2bc424295dd9018ff2a896
SittingDeveloper <rlaaudtlr145@gmail.com>
2023-04-24 16:15:47 +0900

    프록시 설정중

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from './AppRouter';
-import {createRoot} from "react-dom/client";
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -13,7 +11,6 @@ import {createRoot} from "react-dom/client";
 //   </React.StrictMode>
 
 const container = document.getElementById('root');
-
 const root = createRoot(container);
 
 root.render(<AppRouter tab ="home" />);
